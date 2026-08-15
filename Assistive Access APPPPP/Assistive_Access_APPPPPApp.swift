056d0f09cfc3e40beeddfe509357fe90548d5bd0
nishikaagrawal-eng <nishika_agrawal@students.edu.sg>
2026-08-15 16:15:51 +0800

    Initial Commit

---
+++ Assistive Access APPPPP/Assistive_Access_APPPPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assistive_Access_APPPPPApp.swift
+//  Assistive Access APPPPP
+//
+//  Created by Nishika Agrawal on 15/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Assistive_Access_APPPPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
